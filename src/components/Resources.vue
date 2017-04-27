797eda4b0f17c79bc86a384f1ff0cd386ed2f615
vrut57 <vrut57@gmail.com>
2017-04-26 21:20:56 -0400

    adding vue photoswipe package and reverted embedded pdf page

--- src/components/Resources.vue
+++ src/components/Resources.vue
@@ -1,13 +1,11 @@
 <template>
 
 <div class="brochure">
-    <a href="../assets/brochure.doc">Brochure-fact sheet about VRH</a>
+    <a href="brochure.pdf">Brochure-fact sheet about VRH</a>
 
     <div class="brochure-pdf">
-        <object data="../assets/brochure.pdf" type="application/pdf" width="700px" height="700px">
-            <embed src="../assets/brochure.pdf">
-                
-            </embed>
+        <object data="brochure.pdf" type="application/pdf" width="700px" height="700px">
+            <p>Browser does not support embedded PDFs. Please download from the attached link.</p>
         </object>
     </div>
 </div>
