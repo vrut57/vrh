a4013c635b14a092598efd26d52b9aa2bc75adcb
vrut57 <vrut57@gmail.com>
2017-05-03 22:23:30 -0400

    adding external image library; adding working image links to gallery

--- src/components/Gallery.vue
+++ src/components/Gallery.vue
@@ -1,3 +1,42 @@
 <template>
-
+    <div class="gallery-container">
+    <h1>Gallery</h1>
+            <ul>
+            <li v-for="image in images">
+                <a href="" class="js-img-viewer">
+                    <img v-bind:src="image.src"/>
+                </a>
+                <div>
+                    {{image.desc}}
+                </div>
+            </li>
+        </ul>
+    </div>
 </template>
+
+<script>
+    import smartPhoto from 'smartphoto';
+
+    console.log(smartPhoto);
+    document.addEventListener('DOMContentLoaded', function(){
+        new smartPhoto(".js-img-viwer");
+    })
+
+    export default {
+        data() {
+            return {
+            images: [
+                {src: '../static/Fence1.JPG', desc: 'Testing description of image'},
+                {src: '../static/img/Fence1.JPG', desc: 'Here is the second Image'},
+                {src: '../static/img/Steve.JPG', desc: 'online url'},
+                {src: '../static/img/Window.JPG', desc: 'online url'},
+                {src: '../static/img/Volunteers.png', desc: 'online url'},
+                {src: '../static/img/Fence2.JPG', desc: 'online url'},
+                {src: '../static/img/bathroom.png', desc: 'online url'},
+                {src: '../static/img/ramp.png', desc: 'online url'}
+            ]
+        };
+  },
+};
+
+</script>
