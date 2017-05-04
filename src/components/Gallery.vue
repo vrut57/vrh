3197f8f5f8d986a8faa8ae5e1db378605be07520
vrut57 <vrut57@gmail.com>
2017-05-03 22:57:47 -0400

    updating gallery page

--- src/components/Gallery.vue
+++ src/components/Gallery.vue
@@ -3,8 +3,8 @@
     <h1>Gallery</h1>
             <ul>
             <li v-for="image in images">
-                <a href="" class="js-img-viewer">
-                    <img v-bind:src="image.src"/>
+                <a :href="image.src" class="js-img-viewer" :data-caption="image.desc" :data-id="image.id">
+                    <img v-bind:src="image.src" data-group="0"/>
                 </a>
                 <div>
                     {{image.desc}}
@@ -15,25 +15,26 @@
 </template>
 
 <script>
-    import smartPhoto from 'smartphoto';
+    import * as smartPhoto from 'smartphoto';
 
-    console.log(smartPhoto);
     document.addEventListener('DOMContentLoaded', function(){
-        new smartPhoto(".js-img-viwer");
-    })
+        new smartPhoto(".js-img-viwer",{
+            arrows: true     
+        });
+    });
 
     export default {
         data() {
             return {
             images: [
-                {src: '../static/Fence1.JPG', desc: 'Testing description of image'},
-                {src: '../static/img/Fence1.JPG', desc: 'Here is the second Image'},
-                {src: '../static/img/Steve.JPG', desc: 'online url'},
-                {src: '../static/img/Window.JPG', desc: 'online url'},
-                {src: '../static/img/Volunteers.png', desc: 'online url'},
-                {src: '../static/img/Fence2.JPG', desc: 'online url'},
-                {src: '../static/img/bathroom.png', desc: 'online url'},
-                {src: '../static/img/ramp.png', desc: 'online url'}
+                {src: '../static/Fence1.JPG', desc: 'Testing description of image', id: 'id1'},
+                {src: '../static/img/Fence1.JPG', desc: 'Here is the second Image', id: 'id2'},
+                {src: '../static/img/Steve.JPG', desc: 'online url', id: 'id3'},
+                {src: '../static/img/Window.JPG', desc: 'online url', id: 'id4'},
+                {src: '../static/img/Volunteers.png', desc: 'online url', id: 'id5'},
+                {src: '../static/img/Fence2.JPG', desc: 'online url', id: 'id6'},
+                {src: '../static/img/bathroom.png', desc: 'online url', id: 'id7'},
+                {src: '../static/img/ramp.png', desc: 'online url', id: 'id8'}
             ]
         };
   },
